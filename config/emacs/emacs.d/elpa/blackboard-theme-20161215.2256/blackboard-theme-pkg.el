a82396bb3d1cc1154eb785995b8ff1079b0cc3c2
Kamil Ciemniewski <ciemniewski.kamil@gmail.com>
2017-01-29 11:36:15 +0100

    Vim improved

--- config/emacs/emacs.d/elpa/blackboard-theme-20161215.2256/blackboard-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "blackboard-theme" "20161215.2256" "TextMate Blackboard Theme" '((emacs "24")) :url "https://github.com/don9z/blackboard-theme")
--- config/emacs/emacs.d/elpa/reykjavik-theme-20160109.0/reykjavik-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "reykjavik-theme" "20160109.0" "Theme with a dark background." '((emacs "24")))
